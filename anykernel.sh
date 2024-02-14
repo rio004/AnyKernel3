6496595aa8a2342f2868ff8be3264f872bf7ca4e
Rio <57508224+rio004@users.noreply.github.com>
2024-02-14 23:35:02 +0000

    Remove device check

--- anykernel.sh
+++ anykernel.sh
@@ -5,15 +5,12 @@
 # global properties
 properties() { '
 kernel.string=Hydrogen+ by rio113
-do.devicecheck=1
+do.devicecheck=0
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=pissarro
-device.name2=pissarroin
-device.name3=pissarroinpro
-device.name4=pissarropro
+device.name1=
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
